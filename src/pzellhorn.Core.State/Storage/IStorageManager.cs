0d4e6ed05d5fc8a69207051055a0e020132cdac9
Peter Zellhorn - Personal <peterzellhorn@gmail.com>
2025-10-22 06:09:52 +0200

    IStorageManager interface, planned to extend to blob, redis cache, possibly localdisk

---
+++ src/pzellhorn.Core.State/Storage/IStorageManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pzellhorn.Core.State.Storage
+{
+    public interface IStorageManager
+    {
+        public Stream Get(string path, CancellationToken cancellationToken = default);
+        public bool Upsert(string path, Stream content, CancellationToken cancellationToken = default);
+        public bool Delete(string path, CancellationToken cancellationToken = default);
+
+        public bool Exists(string path, CancellationToken cancellationToken = default);
+        public bool Copy(string path, CancellationToken cancellationToken = default);
+        public bool Move(string path, CancellationToken cancellationToken = default);
+    }
+}
