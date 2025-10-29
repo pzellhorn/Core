a03e7aeb78ce6a561b8f33c026bfac40bdace34e
Peter Zellhorn <peterzellhorn@gmail.com>
2025-10-29 05:30:02 +0200

    BaseDbContext now uses explicit constructor so it can be properly derrived in inheriting classes

--- src/pzellhorn.Core.State/Base/DBContext/BaseDbContext.cs
+++ src/pzellhorn.Core.State/Base/DBContext/BaseDbContext.cs
@@ -2,8 +2,9 @@
 
 namespace pzellhorn.Core.State.Base.DBContext;
 
-public partial class BaseDbContext(DbContextOptions<BaseDbContext> options) : DbContext(options)
+public partial class BaseDbContext : DbContext 
 {
+    public BaseDbContext(DbContextOptions options) : base(options) { }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasPostgresExtension("pgcrypto");
