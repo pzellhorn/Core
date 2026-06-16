d427be0cbd244c16e8da709e4369fab33c1b2e3a
Peter Zellhorn <pzellhorn@mobii.com>
2026-06-16 07:50:13 +0200

    AddDiskStorage

--- src/pzellhorn.Core.State/Storage/StorageExtensions.cs
+++ src/pzellhorn.Core.State/Storage/StorageExtensions.cs
@@ -1,16 +1,23 @@
 ﻿using Google.Cloud.Storage.V1;
 using Microsoft.Extensions.DependencyInjection;
 using pzellhorn.Core.State.Storage.Blob;
+using pzellhorn.Core.State.Storage.Disk;
 
 namespace pzellhorn.Core.State.Storage
 {
     public static class StorageExtensions
     {
-        public static IServiceCollection AddStorageExtensions(this IServiceCollection services)
+        public static IServiceCollection AddGoogleBlobStorage(this IServiceCollection services)
         {
             services.AddSingleton<StorageClient>(_ => StorageClient.Create());  //Google Cloud Storage client
-            
+
             services.AddSingleton<IStorageManager, GoogleBlobStorage>();
+            return services;
+        }
+         
+        public static IServiceCollection AddDiskStorage(this IServiceCollection services, string baseStoragePath)
+        {
+            services.AddSingleton<IStorageManager>(_ => new DiskStorage(baseStoragePath));
             return services;
         }
     }
