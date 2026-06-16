c6accb52b57ef132de9438c723b06404a69d7ec8
Peter Zellhorn <pzellhorn@mobii.com>
2026-06-16 08:49:39 +0200

    Separate out these interfasces into their own files

--- src/pzellhorn.Core.Messaging/IQueueConsumer.cs
+++ src/pzellhorn.Core.Messaging/IQueueConsumer.cs
@@ -1,13 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
 namespace pzellhorn.Core.Messaging
 {
-    /// <summary>
-    /// Publishes messages to a named destination (queue/topic). 
-    /// </summary>
-    public interface IQueuePublisher
-    {
-        Task Publish<T>(string destination, T message, CancellationToken cancellationToken = default);
-    } 
-
     /// <summary>
     /// Subscribes to a channel.
     /// Dispose the returned handler to stop consuming.
---
+++ src/pzellhorn.Core.Messaging/IQueuePublisher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pzellhorn.Core.Messaging
+{
+    /// <summary>
+    /// Publishes messages to a named destination (queue/topic). 
+    /// </summary>
+    public interface IQueuePublisher
+    {
+        Task Publish<T>(string destination, T message, CancellationToken cancellationToken = default);
+    }
+}
